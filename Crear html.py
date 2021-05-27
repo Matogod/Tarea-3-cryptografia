5c25ae16b176ac97932c031166b399ff50a4b03f
Matias Torres Gonzalez <74415312+Matogod@users.noreply.github.com>
2021-05-26 21:03:59 -0400

    Add files via upload

---
+++ Crear html.py
@@ -0,0 +1,30 @@
+from twofish import Twofish
+import webbrowser
+
+#Cosas de cifrado
+key = ')H&53,PMyCBuY[72C_Mc'
+x = Twofish(b"""key""")
+y = x.encrypt(b'tengoquetenercar')
+Cifrado = str(y)
+Descifrado = str(x.decrypt(y).decode())
+
+#El Html
+html_archivo = """<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="URF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="wodth=device-width, initial-scale=1.0">
+        <title>Tarea 3 Crypto</title>
+    </head>
+    <body>
+        <p>Este sitio contiene un mensaje secreto</p>
+        <div class="twofish" id="""+Cifrado+"""></div>
+        <div class="key" id="""+key+"""></div>
+    </body>
+</html>
+"""
+
+with open('html_archivo.html', 'w') as f:
+    f.write(str(html_archivo))
+webbrowser.open("html_archivo.html")
